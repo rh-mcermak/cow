3e205b476423c20ae833af1c91c103ad16fab58b
Martin Cermak <mcermak@redhat.com>
2026-04-29 21:07:38 +0200

    Fix cow.repo

--- userinstall.sh
+++ userinstall.sh
@@ -10,10 +10,10 @@ rm -rf ~/.config/waybar
 cp -r cow/config/waybar ~/.config
 popd
 
-cat >cow.repo <<EOF
+cat > cow.repo <<EOF
 [copr:copr.fedorainfracloud.org:mcermak:cow]
 name=Copr repo for cow owned by mcermak
-baseurl=https://download.copr.fedorainfracloud.org/results/mcermak/cow/fedora-$releasever-$basearch/
+baseurl=https://download.copr.fedorainfracloud.org/results/mcermak/cow/fedora-\$releasever-\$basearch/
 type=rpm-md
 skip_if_unavailable=True
 gpgcheck=1
@@ -24,7 +24,7 @@ enabled_metadata=1
 
 [coprdep:https_download_copr_fedorainfracloud_org_results_leloubil_wl_clip_persist_fedora_releasever_basearch]
 name=Copr copr.fedorainfracloud.org/mcermak/cow external runtime dependency #1 - https_download_copr_fedorainfracloud_org_results_leloubil_wl_clip_persist_fedora_releasever_basearch
-baseurl=https://download.copr.fedorainfracloud.org/results/leloubil/wl-clip-persist/fedora-$releasever-$basearch
+baseurl=https://download.copr.fedorainfracloud.org/results/leloubil/wl-clip-persist/fedora-\$releasever-\$basearch
 type=rpm-md
 skip_if_unavailable=True
 repo_gpgcheck=0
