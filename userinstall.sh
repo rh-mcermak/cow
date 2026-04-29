129122f831c69dd45bc747b8a7be217ef6f940f0
Martin Cermak <mcermak@redhat.com>
2026-04-29 20:48:22 +0200

    Update userinstall.sh

--- userinstall.sh
+++ userinstall.sh
@@ -2,4 +2,8 @@
 
 pushd $(mktemp -d)
 git clone https://github.com/rh-mcermak/cow.git
+rm -f ~/.config/cow
+cp cow/config/cow.conf ~/.config/cow
+rm -rf ~/.config/waybar
+cp -r cow/config/waybar ~/.config
 popd
